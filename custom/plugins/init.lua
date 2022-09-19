f2d60e2da41ca51b70dfb72f813d07ce2e74e412
wang <wangjingcun251@gmail.com>
2022-09-19 15:56:02 +0200

    change markdown config

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -61,13 +61,7 @@ return {
     setup = function()
       vim.g.mkdp_filetypes = { "markdown" }
       vim.api.nvim_exec([[
-      let g:mkdp_open_to_the_world = 1
-      let g:mkdp_open_ip = '127.0.0.1' " change to you vps or vm ip
-      let g:mkdp_port = 8080
-      function! g:EchoUrl(url)
-          :echo a:url
-      endfunction
-      let g:mkdp_browserfunc = 'g:EchoUrl'
+      let g:mkdp_browser = 'google-chrome'
       ]], true)
     end,
     ft = { "markdown" },
